29dd850e755f064cfb858fc64efa65d496ffbf49
Elmerh14 <elmerh00@gmail.com>
2025-04-17 19:03:49 -0600

    finished the UI

--- ui.py
+++ ui.py
@@ -1,8 +1,15 @@
-import tkinter as tk
+from tkinter import * 
+from PIL import ImageTk, Image
 
-root = tk.Tk()
+root = Tk()
+root.title("Crash Heatmap")
+root.geometry("1000x700")
 
-label = tk.Label(root, text="Hello World!", font=('Arial', 18))
-label.pack(padx=20, pady=20)
+# # img = Image.open("")
+# img = img.resize((950, 600))
+# tk_img = ImageTk.PhotoImage(img)
+
+# label = Label(root, image=tk_img)
+# label.pack(pady=10)
 
 root.mainloop()
